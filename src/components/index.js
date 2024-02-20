fdad480c617bb9bc3c77b519af9bd42ae63fb53b
Lucy Qi <l8qi@uwaterloo.ca>
2024-02-19 21:48:27 -0500

    removed tech

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas } from './canvas'
 import Hero from './Hero';
 import Navbar from './Navbar';
 import About from './About';
-import Tech from './Tech';
+
 import Works from './Works';
 import Feedbacks from './Feedbacks';
 import Contact from './Contact';
@@ -11,7 +11,7 @@ export {
   Hero,
   Navbar,
   About,
-  Tech,
+
   Works,
   Feedbacks,
   Contact,
