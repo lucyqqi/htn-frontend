e24399f055b7cda04ee7147415712cb9fbf31b17
Lucy Qi <l8qi@uwaterloo.ca>
2024-02-20 18:19:56 -0500

    assets + photos i didnt end up using :(

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,4 @@
 import logo from "./logo.png";
-import backend from "./backend.jpeg";
-import creator from "./creator.jpeg";
-import mobile from "./mobile.jpeg";
-import web from "./web.jpeg";
 
 import menu from "./menu.svg";
 import close from "./close.svg";
