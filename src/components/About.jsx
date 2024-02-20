b2b8f6c5001cabaf97ee6d2762688af0f8b155cb
Lucy Qi <l8qi@uwaterloo.ca>
2024-02-19 20:47:06 -0500

    NEW NEW NEW PLASE WORK

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='object-contain'
+        />
+      </div>
+    </motion.div>
+
+);
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>ARE YOU READY?</p>
+        <h2 className={styles.sectionHeadText}>Canada's Largest Hackathon.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] mx-auto text-center'
+      >
+        This September, join 1,000+ hackers from all around the world for a hackathon like no other. 
+        
+        Discover a community of like-minded hackers, connect with world-class mentors, and build because you love to build. 
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+
+
+    
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
