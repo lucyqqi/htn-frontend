b609a7fe1e28668541d0cdd3f186cf6b99e7880e
Lucy Qi <l8qi@uwaterloo.ca>
2024-02-19 22:00:10 -0500

    deleted useless companies

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,11 +7,6 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
-
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
@@ -26,10 +21,7 @@ export {
   menu,
   close,
 
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+ 
   carrent,
   jobit,
   tripguide,
