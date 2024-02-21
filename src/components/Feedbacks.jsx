ac11fbb5ad51f6e27c77701497fd5ee29c8df6d7
Lucy Qi <l8qi@uwaterloo.ca>
2024-02-20 20:28:08 -0500

    documentation for feedback

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,23 +6,30 @@ import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
+/**
+ * renders individual feedback card.
+ * @param {Object} props component prop
+ * @param {number} index for animation purposes 
+ * @param {string} testimonial testimonial text
+ * @param {string} name name of person giving testinominal
+ * @param {string} designation beginner/advanced hacker
+ * @param {string} image url image of hacker
+ * @returns 
+ */
 const FeedbackCard = ({
   index,
   testimonial,
   name,
   designation,
-
   image,
 }) => (
   <motion.div
-    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    variants={fadeIn("", "spring", index * 0.5, 0.75)} // applies fade-in animation with a delay based on the index
     className='bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full'
   >
     <p className='text-white font-black text-[48px]'>"</p>
-
     <div className='mt-1'>
       <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
-
       <div className='mt-7 flex justify-between items-center gap-1'>
         <div className='flex-1 flex flex-col'>
           <p className='text-white font-medium text-[16px]'>
@@ -32,7 +39,6 @@ const FeedbackCard = ({
             {designation} 
           </p>
         </div>
-
         <img
           src={image}
           alt={`feedback_by-${name}`}
@@ -43,13 +49,15 @@ const FeedbackCard = ({
   </motion.div>
 );
 
+/**
+ * component that renders a collection of feedback cards based on testimonials data
+ * @returns 
+ */
 const Feedbacks = () => {
   return (
     <div className={`mt-12 bg-black-100 rounded-[20px]`}>
-      <div
-        className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}
-      >
-        <motion.div variants={textVariant()}>
+      <div className={`bg-tertiary rounded-2xl ${styles.padding} min-h-[300px]`}>
+        <motion.div variants={textVariant()}> 
           <p className={styles.sectionSubText}>What others say</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
         </motion.div>
